912770639c663864aa69e81d96918c3141af7c4d
rianperez08 <rianperez08@gmail.com>
2023-03-18 15:25:08 +0800

    Shop React

---
+++ src/products.js
@@ -0,0 +1,42 @@
+import product1 from "./assets/products/1.webp";
+import product2 from "./assets/products/2.webp";
+import product3 from "./assets/products/3.webp";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.webp";
+import product7 from "./assets/products/7.webp";
+import product8 from "./assets/products/8.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone 12",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Iphone 12 Pro Max",
+    price: 1099.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Iphone 13",
+    price: 699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Iphone 14 Pro Max",
+    price: 1200.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Iphone 14",
+    price: 1100.0,
+    productImage: product5,
+  },
+
+];
