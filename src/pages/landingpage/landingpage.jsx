89966bf9cbe2a3553148db9796d563e4caf91127
Joseph Lorico <ociroljosephoci@gmail.com>
2023-03-19 22:31:00 +0800

    .

--- src/pages/landingpage/landingpage.jsx
+++ src/pages/landingpage/landingpage.jsx
@@ -11,6 +11,10 @@ const lower = {
     color: 'black',
 };
 
+const higher = {
+    color: 'white',
+};
+
 const divpad = {
     padding: "80px",
 };
@@ -35,81 +39,74 @@ export const LandingPage = () => {
                             <a href="/">
                                 <Button variant="dark">Shop Now</Button>{' '}
                             </a>
-                            <p style={lower}>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            <p style={lower}>Achieve peak experience with the IPhone 14 Pro Max</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
                         className="d-block w-100"
-                        src="carousel/slide1.png"
+                        src="carousel/slide2.png"
                         alt="Second slide"
                         />
 
                         <Carousel.Caption>
                             <a href="/">
-                                <Button variant="dark">Shop Now</Button>{' '}
+                                <Button variant="light">Shop Now</Button>{' '}
+                                <br></br>
                             </a>
-                        <p style={lower}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            <p style={higher}>Stay sleeky with the IPhone 14 Midnight</p>
                         </Carousel.Caption>
                     </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                        className="d-block w-100"
-                        src="carousel/slide1.png"
-                        alt="Third slide"
-                        />
-
-                        <Carousel.Caption>
-                            <a href="/">
-                                <Button variant="dark">Shop Now</Button>{' '}
-                            </a>
-                        <p style={lower}>
-                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                        </p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
+                    
                 </Carousel>
             </div>
 
             
 
             <div style={divpad}>
-            <h2>What's New?</h2>
-            <br></br>
-            <Container>
-                <Row>
-                    <Col sm>
-                        <img id="ip" src="homeprod/6.png" class="d-block w-100"
-                        alt="14pro"/>
-                        <p id="homeprd">IPhone 14 Pro Max 
-                            <br></br>
-                            <a href="/">
-                                <Button variant="primary" id="prdbuy">Buy</Button>{' '}
-                            </a>
-                        </p>
-                        </Col>
-                    <Col sm>
-                        <img id="ip" src="homeprod/4.png" class="d-block w-100"
-                        alt="14pro"/>
-                        <p id="homeprd">IPhone 14 Pro Max
-                            <br></br>
-                            <a href="/">
-                                <Button variant="primary" id="prdbuy">Buy</Button>{' '}
-                            </a>
-                        </p>
-                    </Col>
-                    <Col sm>
-                        <img id="ip" src="homeprod/5.png" class="d-block w-100"
-                        alt="14"/>
-                        <p id="homeprd">Iphone 14
-                            <br></br>
-                            <a href="/">
+                <h2>What's New?</h2>
+                <br></br>
+                <Container>
+                    <Row>
+                        <Col sm>
+                            <img id="ip" src="homeprod/4.png" class="d-block w-100"
+                            alt="14pro"/>
+                            <p id="homeprd">IPhone 14 Pro Max
+                                <br></br>
+                                <a href="/">
                                     <Button variant="primary" id="prdbuy">Buy</Button>{' '}
-                            </a>
-                        </p>
-                    </Col>
-                </Row>
-            </Container>
+                                </a>
+                            </p>
+                        </Col>
+                        <Col sm>
+                            <img id="ip" src="homeprod/5.png" class="d-block w-100"
+                            alt="14"/>
+                            <p id="homeprd">IPhone 14
+                                <br></br>
+                                <a href="/">
+                                        <Button variant="primary" id="prdbuy">Buy</Button>{' '}
+                                </a>
+                            </p>
+                        </Col>
+                        <Col sm>
+                            <img id="ip" src="homeprod/6.png" class="d-block w-100"
+                            alt="14pro"/>
+                            <p id="homeprd">IPhone 13
+                                <br></br>
+                                <a href="/">
+                                    <Button variant="primary" id="prdbuy">Buy</Button>{' '}
+                                </a>
+                            </p>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+
+            <div id="divhelp">
+                <h1 id="help">Need Help?</h1>
+                <a href="/">
+                    <Button variant="light" id="prdbuy">Contact Us</Button>{' '}
+                </a>
             </div>
         </div>;
 
