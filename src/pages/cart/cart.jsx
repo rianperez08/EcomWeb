9cbb6adc7802bcec223a5acadf49412368a084ed
Wrex2432 <wrex2432@gmail.com>
2023-03-23 21:56:57 +0800

    Stff

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { ShopContext } from "../../context/shop-context";
 import { PRODUCTS } from "../../products";
 import { CartItem } from "./cart-item";
@@ -7,11 +7,42 @@ import Button from 'react-bootstrap/Button';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./cart.css";
 
+//Database Stuff
+import { db } from './firebase-config';
+import { collection, doc, getDocs, addDoc, deleteDoc, updateDoc } from "@firebase/firestore";
+
 export const Cart = () => {
   const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
   const totalAmount = getTotalCartAmount();
 
   const navigate = useNavigate();
+
+  //Sent stuff to Database
+  const usersCollectionRef = collection(db, "users");
+  const [users, setUsers] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+
+    const getUsers = async () => {
+        const data = await getDocs(usersCollectionRef);
+        //console.log(data);
+        setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
+    };
+
+    getUsers();
+    {users.map((users) => 
+      users.id == window.localStorage.getItem("ID") ? setTotal(users.total) : setTotal(users.total) 
+    )}
+
+  }, []);
+
+
+  const updateUser = async () => {
+      const userDoc = doc(db, "users", window.localStorage.getItem("ID"));
+      totalAmount += total;
+      await updateDoc(userDoc, { total: (totalAmount)});
+  };
 
   return (
     <div className="cart">
@@ -30,7 +61,7 @@ export const Cart = () => {
         <div className="checkout">
           <p> Subtotal: ${totalAmount} </p>
           <Button variant="dark" onClick={() => navigate("/shop")} id="cshop"> Continue Shopping </Button>
-          <Button variant="dark" onClick={() => { checkout(); navigate("/confirmation"); }} id="checkout" >
+          <Button variant="dark" onClick={() => { checkout(); updateUser(); navigate("/confirmation"); }} id="checkout" >
             {" "} Checkout {" "}
           </Button>
         </div>
