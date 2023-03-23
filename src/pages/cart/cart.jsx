d3b722d766b18a38143e7f0191ca8662e0764b03
Joseph Lorico <ociroljosephoci@gmail.com>
2023-03-23 13:32:06 +0800

    productpage

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -3,8 +3,8 @@ import { ShopContext } from "../../context/shop-context";
 import { PRODUCTS } from "../../products";
 import { CartItem } from "./cart-item";
 import { useNavigate } from "react-router-dom";
-
 import "./cart.css";
+
 export const Cart = () => {
   const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
   const totalAmount = getTotalCartAmount();
@@ -12,35 +12,37 @@ export const Cart = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="cart">
-      <div>
-        <h1>Your Cart Items</h1>
-      </div>
+    <div className="body">
       <div className="cart">
-        {PRODUCTS.map((product) => {
-          if (cartItems[product.id] !== 0) {
-            return <CartItem data={product} />;
-          }
-        })}
-      </div>
-
-      {totalAmount > 0 ? (
-        <div className="checkout">
-          <p> Subtotal: ${totalAmount} </p>
-          <button onClick={() => navigate("/")}> Continue Shopping </button>
-          <button
-            onClick={() => {
-              checkout();
-              navigate("/checkout");
-            }}
-          >
-            {" "}
-            Checkout{" "}
-          </button>
+        <div>
+          <h1>Your Cart Items</h1>
         </div>
-      ) : (
-        <h1> Your Shopping Cart is Empty</h1>
-      )}
+        <div className="cart">
+          {PRODUCTS.map((product) => {
+            if (cartItems[product.id] !== 0) {
+              return <CartItem data={product} />;
+            }
+          })}
+        </div>
+
+        {totalAmount > 0 ? (
+          <div className="checkout">
+            <p> Subtotal: ${totalAmount} </p>
+            <button onClick={() => navigate("/")}> Continue Shopping </button>
+            <button
+              onClick={() => {
+                checkout();
+                navigate("/checkout");
+              }}
+            >
+              {" "}
+              Checkout{" "}
+            </button>
+          </div>
+        ) : (
+          <h1> Your Shopping Cart is Empty</h1>
+        )}
+      </div>
     </div>
   );
 };
