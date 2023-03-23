d0b3763af6c6f0be86c74bc5c37fc39b822b1c8a
Joseph Lorico <ociroljosephoci@gmail.com>
2023-03-23 13:45:01 +0800

    productpage

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -6,10 +6,11 @@ import "./shop.css";
 export const Shop = () => {
   return (
     <div className="shop">
-      <div className="shopTitle">
-        <h1>KALARO TECH SHOP</h1>
+      <div className="shoptitlediv">
+        <div className="shopTitle">
+          <h1>KALARO TECH SHOP</h1>
+        </div>
       </div>
-      <hr id="line"></hr>
 
       <div className="products">
         {PRODUCTS.map((product) => (
